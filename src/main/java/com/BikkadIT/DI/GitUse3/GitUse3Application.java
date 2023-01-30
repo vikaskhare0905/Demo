1b620ec48d0a3616b6a6078dcbbab59aa29f4c45
vikaskhare0905 <vikaskharevk1994@gmail.com>
2023-01-30 19:45:26 +0530

    changes by monty

---
+++ src/main/java/com/BikkadIT/DI/GitUse3/GitUse3Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.DI.GitUse3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitUse3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitUse3Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/BikkadIT/DI/GitUse3/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.DI.GitUse3;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GitUse3Application.class);
+	}
+
+}
